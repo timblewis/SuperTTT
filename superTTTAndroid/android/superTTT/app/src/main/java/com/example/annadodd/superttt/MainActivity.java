9466357112d49989ecedc2167f71f4a6a5436ada
annadodd <annadodd@umich.edu>
2016-02-28 18:44:14 -0500

    implementing start game interface in android developer proj

--- superTTTAndroid/android/superTTT/app/src/main/java/com/example/annadodd/superttt/MainActivity.java
+++ superTTTAndroid/android/superTTT/app/src/main/java/com/example/annadodd/superttt/MainActivity.java
@@ -8,15 +8,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+    private View i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newGame = new Button(this);
+        newGame.setText("New Game");
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
